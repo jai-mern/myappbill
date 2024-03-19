54d5a6ca1bc8b86cb07117fb80ee99290cfc26c5
jai-mern <Kjaya8862@gamail.com>
2024-03-20 01:43:06 +0530

    final comit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+/**
+ *
+ * app.js
+ *
+ */
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from 'react';
+import { Provider } from 'react-redux';
+import { ConnectedRouter } from 'connected-react-router';
+
+import store, { history } from './store';
+import { SocketProvider } from './contexts/Socket';
+import { SET_AUTH } from './containers/Authentication/constants';
+import Application from './containers/Application';
+import ScrollToTop from './scrollToTop';
+import setToken from './utils/token';
+
+// Import application sass styles
+import './styles/style.scss';
+
+// Import Font Awesome Icons Set
+import 'font-awesome/css/font-awesome.min.css';
+
+// Import Simple Line Icons Set
+import 'simple-line-icons/css/simple-line-icons.css';
+
+// react-bootstrap-table2 styles
+import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css';
+
+// rc-slider style
+import 'rc-slider/assets/index.css';
+
+// Authentication
+const token = localStorage.getItem('token');
+
+if (token) {
+  // authenticate api authorization
+  setToken(token);
+
+  // authenticate routes
+  store.dispatch({ type: SET_AUTH });
 }
 
-export default App;
+const app = () => (
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <SocketProvider>
+        <ScrollToTop>
+          <Application />
+        </ScrollToTop>
+      </SocketProvider>
+    </ConnectedRouter>
+  </Provider>
+);
+
+export default app;
